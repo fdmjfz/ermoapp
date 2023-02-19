ec1468cc4f63037feb603cd3732a490935f2ccf2
veuc7913 <fejofefe@hotmail.es>
2023-02-19 12:14:15 +0100

    Update

--- utils/hc12.py
+++ utils/hc12.py
@@ -2,11 +2,7 @@ import serial
 
 
 class ermo_hc12:
-    def __init__(self, serial_port, baud_rate):
-        if not serial_port:
-            serial_port = '/dev/ttyS0'
-        if not baud_rate:
-            baud_rate = 9600
+    def __init__(self, serial_port='/dev/ttyS0', baud_rate=9600):
 
         self.serial = serial.Serial(
             port=serial_port,
