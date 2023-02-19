623657269823b5a173c3099d83daef0145940f98
veuc7913 <fejofefe@hotmail.es>
2023-02-19 12:12:41 +0100

    Update

--- utils/hc12.py
+++ utils/hc12.py
@@ -11,4 +11,23 @@ class ermo_hc12:
         self.serial = serial.Serial(
             port=serial_port,
             baudrate=baud_rate,
+            timeout=10,
         )
+
+    def receive(self):
+        x = ser.read_until(bytes('>', encoding='utf-8'))
+        message = x.decode('utf-8')
+        message = message.replace('>', '')
+
+        items_list = message.split('~')
+
+        device_info = items_list[0].split(',')
+        device_id = device_info[0]
+        device_type = device_info[1]
+
+        text = items_list[1]
+
+        if device_type == 'u':
+            output = f'{device_id}: {text}'
+
+            return output
